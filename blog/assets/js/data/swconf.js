839789614b309ec3f0c4932a03c0cf13d0096958
Gabriel-Gebril <gebril.gabriel@gmail.com>
2024-11-23 23:58:01 -0500

    posted a placeholder

--- blog/assets/js/data/swconf.js
+++ blog/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1732422759',resources: [
+    cacheName: 'chirpy-1732424217',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
