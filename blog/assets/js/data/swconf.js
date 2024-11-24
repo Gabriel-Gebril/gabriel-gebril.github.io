84f2c7bae45799141f45f1059540ff32879c407d
Gabriel-Gebril <gebril.gabriel@gmail.com>
2024-11-23 23:33:40 -0500

    test page

--- blog/assets/js/data/swconf.js
+++ blog/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1732420998',resources: [
+    cacheName: 'chirpy-1732422759',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
