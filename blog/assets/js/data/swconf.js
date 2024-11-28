886cf24c42ccc8ae378b9ed9a8c99916969951e4
Gabriel-Gebril <gebril.gabriel@gmail.com>
2024-11-28 12:27:12 -0500

    Fixed twitter and github links

--- blog/assets/js/data/swconf.js
+++ blog/assets/js/data/swconf.js
@@ -1,1 +1,1 @@
-const swconf = { cacheName: 'chirpy-1732814260',resources: [ '/blog/assets/css/jekyll-theme-chirpy.css', '/blog/', '/blog/categories/', '/blog/tags/', '/blog/archives/', '/blog/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
+const swconf = { cacheName: 'chirpy-1732814805',resources: [ '/blog/assets/css/jekyll-theme-chirpy.css', '/blog/', '/blog/categories/', '/blog/tags/', '/blog/archives/', '/blog/about/', ], interceptor: {paths: [ ],urlPrefixes: [ ] }, purge: false };
